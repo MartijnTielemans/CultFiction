2533a16d9396a3b553582966632f0e856c0a021d
Martijn <martijn.tielemans@live.nl>
2021-10-01 10:09:35 +0200

    Changed Scripts
    - Deleted 2D array

--- Classic Game Box Sorter/Assets/Scripts/GridManager.cs
+++ Classic Game Box Sorter/Assets/Scripts/GridManager.cs
@@ -1,17 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 public class GridManager : MonoBehaviour
 {
     public GameObject node;
 
     GameObject[,] nodes;
-    public Array2D<bool> nodeMap;
-    [SerializeField] int rows, cols;
+    [SerializeField] int xSize, ySize;
     [SerializeField] float rowSpacing, colSpacing;
 
     private void Awake()
@@ -21,11 +17,11 @@ public class GridManager : MonoBehaviour
 
     private void Start()
     {
-        nodes = new GameObject[cols, rows];
+        nodes = new GameObject[ySize, xSize];
 
-        for (int c = 0; c < cols; c++)
+        for (int c = 0; c < ySize; c++)
         {
-            for (int r = 0; r < rows; r++)
+            for (int r = 0; r < xSize; r++)
             {
                 if (HasNode(c, r))
                 {
@@ -42,7 +38,8 @@ public class GridManager : MonoBehaviour
 
     bool HasNode(int cols, int rows)
     {
-        return nodeMap[cols, rows];
+        // Check if node map has a node
+        return true;
     }
 
     Vector2 MakeNodePosition(float currentRow, float currentCol, float rSpacing, float cSpacing)
@@ -55,73 +52,3 @@ public class GridManager : MonoBehaviour
         return nodes;
     }
 }
-
-#if UNITY_EDITOR
-[CustomEditor(typeof(GridManager)), CanEditMultipleObjects]
-public class GridManagerEditor : Editor
-{
-    private SerializedProperty rows, cols;
-    private SerializedProperty rowSpacing, colSpacing;
-    private SerializedProperty node;
-    private SerializedProperty nodeMap;
-
-
-    void OnEnable()
-    {
-        rows = serializedObject.FindProperty("rows");
-        cols = serializedObject.FindProperty("cols");
-        rowSpacing = serializedObject.FindProperty("rowSpacing");
-        colSpacing = serializedObject.FindProperty("colSpacing");
-        node = serializedObject.FindProperty("node");
-        nodeMap = serializedObject.FindProperty("nodeMap");
-    }
-
-    public override void OnInspectorGUI()
-    {
-        GridManager obj = (GridManager)target;
-        serializedObject.Update();
-        EditorGUILayout.PropertyField(node);
-        EditorGUILayout.PropertyField(rows);
-        EditorGUILayout.PropertyField(cols);
-        EditorGUILayout.PropertyField(rowSpacing);
-        EditorGUILayout.PropertyField(colSpacing);
-        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-        EditorGUILayout.LabelField("Node map");
-
-        //if (obj.nodeMap == null || obj.nodeMap.GetLength(0) != rows.intValue || obj.nodeMap.GetLength(1) != cols.intValue)
-        //{
-        //    obj.nodeMap = new Array2D<bool>(rows.intValue, cols.intValue);
-
-        //    for (int c = 0; c < cols.intValue; c++)
-        //    {
-        //        for (int r = 0; r < rows.intValue; r++)
-        //        {
-        //            obj.nodeMap[c, r] = true;
-        //        }
-        //    }
-        //}
-        EditorGUILayout.PropertyField(nodeMap);
-        serializedObject.ApplyModifiedProperties();
-        GUILayout.BeginVertical(GUI.skin.box);
-
-        //obj.nodeMap.Cols = cols.intValue;
-        //obj.nodeMap.Rows = rows.intValue;
-
-        //for (int c = 0; c < cols.intValue; c++)
-        //{
-        //    GUILayout.BeginHorizontal();
-        //    for (int r = 0; r < rows.intValue; r++)
-        //    {
-        //        obj.nodeMap[c, r] = GUILayout.Toggle(obj.nodeMap[c, r], "");
-        //    }
-        //    GUILayout.EndHorizontal();
-        //}
-        
-        GUILayout.EndVertical();
-
-        
-
-        
-    }
-}
-#endif
